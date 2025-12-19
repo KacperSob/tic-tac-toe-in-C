eaed8af66cea51fa07fa02274c1e1c3d3af09e70
KacperSob <kacper.sobania6@gmail.com>
2025-12-19 15:03:38 +0100

    finished tests

--- lib/tests.c
+++ lib/tests.c
@@ -19,6 +19,17 @@ bool _runAllTests(bool shouldDisplayInformation){
             output = false;
         }
         printf("test3 %d\n", test);
+        test = test4();
+        if(!test){
+            output = false;
+        }
+        printf("test4 %d\n", test);
+        test = test5();
+        if(!test){
+            output = false;
+        }
+        printf("test5 %d\n", test);
+
 
 
         return output;
@@ -103,6 +114,44 @@ bool test3(){
     }
     fillTable(table, ' ');
 
+    destroyTable(table);
+    return output;
+}
+bool test4(){
+    char** table = createTable();
+    bool output = true;
+    table[0][0] = 'x';
+    table[0][2] = 'o';
+    table[1][0] = 'o';
+    table[1][1] = 'o';
+    table[2][0] = 'x';
+    table[2][1] = 'x';
+    table[2][2] = 'x';
+    if(checkWhoWon(table) != 'x'){
+        output = false;
+    }
+    fillTable(table, ' ');
+    table[0][0] = 'o';
+    table[0][1] = 'x';
+    table[0][2] = 'o';
+    table[1][0] = 'o';
+    table[1][1] = 'x';
+    table[1][2] = 'x';
+    table[2][0] = 'x';
+    table[2][1] = 'o';
+    table[2][2] = 'o';
+    if(checkWhoWon(table) != '-'){
+        output = false;
+    }
+    destroyTable(table);
+    return output;
+}
+bool test5(){
+    char** table = createTable();
+    bool output = true;
+    if(checkWhoWon(table) != '-'){
+        output = false;
+    }
     destroyTable(table);
     return output;
 }
